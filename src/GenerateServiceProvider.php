fd5c3a45e38353b94f9c347e5d2da5452713f3f3
Ngo Huu Nhut <ngohuunhut16@gmail.com>
2022-02-25 13:10:17 +0700

    wip

---
+++ src/GenerateServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Nhn\Generate;
+
+use Illuminate\Support\ServiceProvider;
+use Nhn\Generate\Console\Commands\GenerateCommand;
+
+class GenerateServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->configureCommands();
+    }
+
+    /**
+     * Configure the commands offered by the application.
+     *
+     * @return void
+     */
+    protected function configureCommands()
+    {
+        if (! $this->app->runningInConsole()) {
+            return;
+        }
+
+        $this->commands([
+            GenerateCommand::class,
+        ]);
+    }
+}
